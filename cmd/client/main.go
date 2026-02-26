299f56e5160f3e02a449caee2ea34883d6e524e2
Akansha Roy <akansharoy2008@gmail.com>
2026-02-27 00:12:40 +0530

    implemented persistent connection of the client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -8,6 +8,10 @@ import (
 
 func main() {
 	client := transport.NewTCPClient(":8080")
+	if err := client.Connect(); err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
 	err := client.Send("meowww from client!\nYOKOSO PIRAA PIRAA PUKKI PUKKKI WAKU WAKU WOSHAIIII")
 	err = client.Send("YOWAIII MOOO!!!")
 	err = client.Send("ARAARRAA GOMEENNN!!!")
--- internal/transport/tcp_client.go
+++ internal/transport/tcp_client.go
@@ -1,6 +1,7 @@
 package transport
 
 import (
+	"errors"
 	"fmt"
 	"net"
 
@@ -9,6 +10,7 @@ import (
 
 type TCPClient struct {
 	addr string
+	conn net.Conn
 }
 
 func NewTCPClient(addr string) *TCPClient {
@@ -17,26 +19,39 @@ func NewTCPClient(addr string) *TCPClient {
 	}
 }
 
-func (c *TCPClient) Send(message string) error {
+func (c *TCPClient) Connect() error {
 	conn, err := net.Dial("tcp", c.addr)
-
 	if err != nil {
 		return err
 	}
-	defer conn.Close()
+	c.conn = conn
+	return nil
+}
 
-	err = protocol.WriteFrame(conn, []byte(message))
+func (c *TCPClient) Send(message string) error {
+	if c.conn == nil {
+		return errors.New("not connected")
+	}
+
+	err := protocol.WriteFrame(c.conn, []byte(message))
 
 	if err != nil {
 		return err
 	}
 
 	// buffer := make([]byte, 1024)
-	payload, err := protocol.ReadFrame(conn)
+	payload, err := protocol.ReadFrame(c.conn)
 
 	if err != nil {
 		return err
 	}
 	fmt.Println("Echoed response:", string(payload))
+	return nil
+}
+
+func (c *TCPClient) Close() error {
+	if c.conn != nil {
+		c.conn.Close()
+	}
 	return nil
 }
