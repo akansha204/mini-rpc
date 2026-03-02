24ce91beecb0e3fd6eff7b17f80c02abd1c71d47
Akansha Roy <akansharoy2008@gmail.com>
2026-03-02 16:11:01 +0530

    req and res structs

--- internal/protocol/messaging.go
+++ internal/protocol/messaging.go
@@ -1,1 +1,13 @@
 package protocol
+
+type Request struct {
+	ID     uint64        `json:"id"`
+	Method string        `json:"method"`
+	Params []interface{} `json:"params"`
+}
+
+type Response struct {
+	ID     uint64      `json:"id"`
+	Result interface{} `json:"result"`
+	Error  string      `json:"error"`
+}
