3ea20f7ec27f5ef92c3867bf41b11687a183c6ab
Akansha Roy <akansharoy2008@gmail.com>
2026-03-05 21:04:49 +0530

    client and handler of rpc

--- internal/rpc/client.go
+++ internal/rpc/client.go
@@ -1,1 +1,59 @@
 package rpc
+
+import (
+	"errors"
+
+	"github.com/akansha204/mini-rpc/internal/codec"
+	"github.com/akansha204/mini-rpc/internal/protocol"
+	"github.com/akansha204/mini-rpc/internal/transport"
+)
+
+type Client struct {
+	codec codec.Codec
+	conn  *transport.TCPClient
+	id    uint64
+}
+
+func NewClient(c codec.Codec, conn *transport.TCPClient) *Client {
+	return &Client{
+		codec: c,
+		conn:  conn,
+	}
+}
+func (c *Client) Call(method string, params []interface{}, result interface{}) error {
+	c.id++
+
+	req := protocol.Request{
+		ID:     c.id,
+		Method: method,
+		Params: params,
+	}
+
+	data, err := c.codec.Encode(req)
+	if err != nil {
+		return err
+	}
+
+	err = c.conn.Send(data)
+	if err != nil {
+		return err
+	}
+
+	respBytes, err := c.conn.Receive()
+	if err != nil {
+		return err
+	}
+
+	var resp protocol.Response
+	err = c.codec.Decode(respBytes, &resp)
+	if err != nil {
+		return err
+	}
+
+	if resp.Error != "" {
+		return errors.New(resp.Error)
+	}
+	data, _ = c.codec.Encode(resp.Result)
+	return c.codec.Decode(data, result)
+
+}
--- internal/rpc/handler.go
+++ internal/rpc/handler.go
@@ -1,1 +1,47 @@
 package rpc
+
+import "fmt"
+
+type Calculator struct{}
+
+func (c *Calculator) Add(params []interface{}) (interface{}, error) {
+	if len(params) != 2 {
+		return nil, fmt.Errorf("Add expects 2 parameters")
+	}
+
+	a := int(params[0].(float64))
+	b := int(params[1].(float64))
+
+	return a + b, nil
+}
+
+func (c *Calculator) Subtract(params []interface{}) (interface{}, error) {
+	if len(params) != 2 {
+		return nil, fmt.Errorf("Subtract expects 2 parameters")
+	}
+
+	a := int(params[0].(float64))
+	b := int(params[1].(float64))
+
+	return a - b, nil
+}
+func (c *Calculator) Mul(params []interface{}) (interface{}, error) {
+	if len(params) != 2 {
+		return nil, fmt.Errorf("Mul expects 2 parameters")
+	}
+	a := int(params[0].(float64))
+	b := int(params[1].(float64))
+	return a * b, nil
+}
+
+func (c *Calculator) Div(params []interface{}) (interface{}, error) {
+	if len(params) != 2 {
+		return nil, fmt.Errorf("Div expects 2 parameters")
+	}
+	a := int(params[0].(float64))
+	b := int(params[1].(float64))
+	if b == 0 {
+		return nil, fmt.Errorf("division by zero is not possible")
+	}
+	return (float64)(a / b), nil
+}
