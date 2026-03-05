9c0fc488f23f2d7cd14390fe3d7d047ad803d7a2
Akansha Roy <akansharoy2008@gmail.com>
2026-03-05 14:48:22 +0530

    registry.go done to register all the methods

--- internal/rpc/registry.go
+++ internal/rpc/registry.go
@@ -1,1 +1,22 @@
 package rpc
+
+type HandlerFunc func(params []interface{}) (interface{}, error)
+
+type Registry struct {
+	methods map[string]HandlerFunc
+}
+
+func NewRegistry() *Registry {
+	return &Registry{
+		methods: make(map[string]HandlerFunc),
+	}
+}
+
+func (r *Registry) Register(name string, handler HandlerFunc) {
+	r.methods[name] = handler
+}
+
+func (r *Registry) Get(name string) (HandlerFunc, bool) {
+	h, ok := r.methods[name]
+	return h, ok
+}
